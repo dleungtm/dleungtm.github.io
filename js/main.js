d684d6e9eb9ff720d331c0e37821b2fa20c4d194
Desmond Leung <d.leung@live.ca>
2017-01-16 00:38:19 -0800

    Updated content
    - updated resume
    - updated Mind the Gap thumbnail
    - remove broken contact form

--- js/main.js
+++ js/main.js
@@ -214,33 +214,33 @@ jQuery(function($){
 	Twitter Feed
 	================================================== */
 
-	BRUSHED.tweetFeed = function(){
+	// BRUSHED.tweetFeed = function(){
 
-		var valueTop = -64; // Margin Top Value
+	// 	var valueTop = -64; // Margin Top Value
 
-		$("#ticker").tweet({
-			modpath: 'js/twitter/',
-			username: "dleungtm", // Change this with YOUR ID
-			page: 1,
-			avatar_size: 0,
-			count: 10,
-			template: "{text}{time}",
-			filter: function(t){ return ! /^@\w+/.test(t.tweet_raw_text); },
-			loading_text: "loading ..."
-		}).bind("loaded", function() {
-			var ul = $(this).find(".tweet_list");
-			var ticker = function() {
-				setTimeout(function() {
-					ul.find('li:first').animate( {marginTop: valueTop + 'px'}, 500, 'linear', function() {
-						$(this).detach().appendTo(ul).removeAttr('style');
-					});
-					ticker();
-				}, 5000);
-			};
-			ticker();
-		});
+	// 	$("#ticker").tweet({
+	// 		modpath: 'js/twitter/',
+	// 		username: "dleungtm", // Change this with YOUR ID
+	// 		page: 1,
+	// 		avatar_size: 0,
+	// 		count: 10,
+	// 		template: "{text}{time}",
+	// 		filter: function(t){ return ! /^@\w+/.test(t.tweet_raw_text); },
+	// 		loading_text: "loading ..."
+	// 	}).bind("loaded", function() {
+	// 		var ul = $(this).find(".tweet_list");
+	// 		var ticker = function() {
+	// 			setTimeout(function() {
+	// 				ul.find('li:first').animate( {marginTop: valueTop + 'px'}, 500, 'linear', function() {
+	// 					$(this).detach().appendTo(ul).removeAttr('style');
+	// 				});
+	// 				ticker();
+	// 			}, 5000);
+	// 		};
+	// 		ticker();
+	// 	});
 
-	}
+	// }
 
 
 	/* ==================================================
@@ -402,17 +402,7 @@ jQuery(function($){
 		Modernizr.load([
 			{
 				test: Modernizr.placeholder,
-				nope: 'js/placeholder.js',
-				complete : function() {
-					if (!Modernizr.placeholder) {
-						Placeholders.init({
-							live: true,
-							hideOnFocus: false,
-							className: "yourClass",
-							textColor: "#999"
-						});
-					}
-				}
+				nope: 'js/placeholder.js'
 			}
 		]);
 
